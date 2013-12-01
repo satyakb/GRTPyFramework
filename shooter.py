2858b760c731536b68348ebc233856580de1837e
ajnam12 <ajnamavari12@gmail.com>
2013-12-01 12:29:24 -0800

    Moved file

--- shooter.py
+++
@@ -1,9 +0,0 @@
-#Ball shooter for the mock build shop project
-class Shooter:
-    def __init__(self, fly1, fly2):
-        self.fly1 = fly1
-        self.fly2 = fly2
-    def set_speed(self, speed)
-        self.fly1.Set(speed)
-        self.fly2.Set(speed)
-    #More stuff
